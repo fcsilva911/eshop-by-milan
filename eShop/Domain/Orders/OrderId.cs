326e2576606ffbc06c22c43bf6e17b9aff4528a4
ovector68o <fc.silva.911@gmail.com>
2023-10-13 23:57:05 +0100

    add domain entities

---
+++ eShop/Domain/Orders/OrderId.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Orders
+{
+    public class OrderId
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ eShop/Domain/Products/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Products
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetByIdAsync(ProductId id);
+
+        void Add(Product product);
+
+        void Update(Product product);
+
+        void Remove(Product product);
+    }
+}
